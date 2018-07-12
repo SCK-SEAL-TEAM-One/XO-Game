43b5375d8cf73acce75b683f22cf9e6fa399cc98
teerawat.th <teerawat.th@baac.or.th>
2018-07-12 15:15:11 +0700

    [Added]: เพิ่มฟังชัน Test_SetPlayerSequency_Input_Jay_Aun_Should_Be_Struct_Players

--- horizontal-win/src/xo/xo_test.go
+++ horizontal-win/src/xo/xo_test.go
@@ -1,1 +1,29 @@
 package xo
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_SetPlayerSequency_Input_Jay_Aun_Should_Be_Struct_Players(t *testing.T) {
+	playerName1 := "JAY"
+	playerName2 := "AUN"
+	expected := []Player{
+		{
+			Name:     "JAY",
+			Symbol:   "O",
+			Sequence: "FIRST",
+		},
+		{
+			Name:     "AUN",
+			Symbol:   "X",
+			Sequence: "SECOND",
+		},
+	}
+
+	actual := SetPlayerSequency(playerName1, playerName2)
+
+	if fmt.Sprintf("%v", expected) != fmt.Sprintf("%v", actual) {
+		t.Errorf("expected %v but got %v", expected, actual)
+	}
+}
