a9e0d774f603c7e0b735dd2604e2f481eae3d4f9
nat.la <nat.la@baac.or.th>
2018-07-12 14:55:20 +0700

    [Created]:ร้างฟังก์ชั่น Test_InputPlayerX_Input_7_Should_Be_True และฟังก์ชั่น InputPlayerX และ Getcell

--- diagonal-win/src/xo/xo.go
+++ diagonal-win/src/xo/xo.go
@@ -5,7 +5,6 @@ type XOGAME struct{
 	Round int
 	MaxRound int
 }
- 
 func NewXOGame() XOGAME{
 	return XOGAME{MaxRound:9} 
 } 
@@ -15,6 +14,29 @@ type Cell struct {
 	Col int
 }
 func (xo *XOGAME) InputPlayerO(number int) bool{
+	cell := getCell(number)
+	row := cell.Row
+	col := cell.Col
+	if xo.Table[row][col] == "" {
+		xo.Table[row][col] = "O"
+		return true
+	}
+	return false
+
+}
+
+func (xo *XOGAME) InputPlayerX(number int) bool{
+	cell := getCell(number)
+	row := cell.Row
+	col := cell.Col
+	if xo.Table[row][col] == "" {
+		xo.Table[row][col] = "X"
+		return true
+	}
+	return false
+}
+
+func getCell(number int) Cell{
 	numberCell := map[int] Cell{
 		1: Cell{ Row: 0, Col: 0 },
 		2: Cell{ Row: 0, Col: 1 },
@@ -25,14 +47,6 @@ func (xo *XOGAME) InputPlayerO(number int) bool{
 		7: Cell{ Row: 2, Col: 0 },
 		8: Cell{ Row: 2, Col: 1 },
 		9: Cell{ Row: 2, Col: 2 },
-	} 
-	
-	row := numberCell[number].Row
-	col := numberCell[number].Col
-	if xo.Table[row][col] == "" {
-		xo.Table[row][col] = "O"
-		return true
-	}
-	return false
-
+	}  
+	return numberCell[number]
 }
--- diagonal-win/src/xo/xo_test.go
+++ diagonal-win/src/xo/xo_test.go
@@ -14,3 +14,13 @@ func Test_InputPlayerO_Input_5_Should_Be_True(t *testing.T){
 		t.Errorf("Should be %v but got %v %v",expected, actual, xo.Table)
 	}
 }
+
+func Test_InputPlayerX_Input_7_Should_Be_True(t *testing.T){
+	input := 7
+	expected := true
+	xo := NewXOGame() 
+	actual := xo.InputPlayerX(input) 
+	if expected != actual {
+		t.Errorf("Should be %v but got %v %v",expected, actual, xo.Table)
+	}
+}
