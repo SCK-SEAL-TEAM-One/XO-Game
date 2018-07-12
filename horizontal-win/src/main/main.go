7f56ea991000a6775e314398b3164128140dad87
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-07-12 16:55:35 +0700

    [Edited]: ไฟล์ main.go

--- horizontal-win/src/main/main.go
+++ horizontal-win/src/main/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"xo"
+)
 
+func main() {
+	xogame := xo.XOGame{}
+	var playerName1, playerName2 string
+	fmt.Scanf("%s %s", &playerName1, &playerName2)
+	fmt.Println(xogame.SetPlayerSequency(playerName1, playerName2))
+
+	var currentPlayerName string
+	var positionNumber int
+	for step := 0; step <= xo.GAMEONEROUND; step++ {
+		fmt.Scanf("%s %d", &currentPlayerName, &positionNumber)
+		chooseResult := xogame.ChoosePosition(currentPlayerName, positionNumber)
+		fmt.Println(chooseResult)
+		if xogame.GetPlayerWin(xogame.Step) {
+			fmt.Println("WIN!")
+			break
+		}
+	}
 }
