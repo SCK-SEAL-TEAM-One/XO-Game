2f2032055430312791e2fffa2aacaedc711f5c1a
golfgabkung <golfgabkung@gmail.com>
2018-07-12 15:27:26 +0700

    [Edited]: สร้างฟั

--- vertical-win/src/vertical/vertical.go
+++ vertical-win/src/vertical/vertical.go
@@ -1,1 +1,14 @@
 package vertical
+
+import (
+	"XO-Game/vertical-win/src/model"
+)
+
+func DuplicateMark(row int, column int, board []model.Cell) bool {
+	for _, cellValue := range board {
+		if cellValue.Row == row && cellValue.Column == column {
+			return true
+		}
+	}
+	return false
+}
--- vertical-win/src/vertical/vertical_test.go
+++ vertical-win/src/vertical/vertical_test.go
@@ -1,1 +1,20 @@
 package vertical
+
+import (
+	"XO-Game/vertical-win/src/model"
+	"testing"
+)
+
+func Test_DuplicateMark_Input_Row_1_Column_1_Should_Be_False(t *testing.T) {
+	row := 1
+	column := 1
+	board := []model.Cell{}
+	board = append(board, model.Cell{Row: 1, Column: 2, MarkedPlayer: "Player1"})
+	expected := false
+
+	actual := DuplicateMark(row, column, board)
+
+	if expected != actual {
+		t.Errorf("Should be %t but got %t", expected, actual)
+	}
+}
