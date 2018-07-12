e1f7c3734a78c2b6d1a773a5fb97e427d10473c1
golfgabkung <golfgabkung@gmail.com>
2018-07-12 15:32:00 +0700

    [Edited]: เพิ่มฟังก์ชันทดสอบ DuplicateMark ใสไฟล์ vertical_test.go

--- vertical-win/src/vertical/vertical_test.go
+++ vertical-win/src/vertical/vertical_test.go
@@ -18,3 +18,17 @@ func Test_DuplicateMark_Input_Row_1_Column_1_Should_Be_False(t *testing.T) {
 		t.Errorf("Should be %t but got %t", expected, actual)
 	}
 }
+
+func Test_DuplicateMark_Input_Row_1_Column_1_Should_Be_True(t *testing.T) {
+	row := 1
+	column := 1
+	board := []model.Cell{}
+	board = append(board, model.Cell{Row: 1, Column: 1, MarkedPlayer: "Player1"})
+	expected := true
+
+	actual := DuplicateMark(row, column, board)
+
+	if expected != actual {
+		t.Errorf("Should be %t but got %t", expected, actual)
+	}
+}
