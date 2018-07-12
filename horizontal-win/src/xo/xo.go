23fc209af9c7bf6218918da2e842fae74036999d
manan <aoy.mananchaya@gmail.com>
2018-07-12 15:17:30 +0700

    [Added]: func Test Name : Test_ChoosePosition_Input_Jay_1_Aun_5_Should_be_Array_15 and function ChoosePosition

--- horizontal-win/src/xo/xo.go
+++ horizontal-win/src/xo/xo.go
@@ -1,1 +1,10 @@
 package xo
+
+type XOGame struct {
+	Step []int
+}
+
+func (xo *XOGame) ChoosePosition(playerName string, position int) []int {
+	xo.Step = append(xo.Step, position)
+	return xo.Step
+}
--- horizontal-win/src/xo/xo_test.go
+++ horizontal-win/src/xo/xo_test.go
@@ -1,1 +1,18 @@
 package xo
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_ChoosePosition_Input_Jay_1_Aun_5_Should_be_Array_15(t *testing.T) {
+	xo := XOGame{}
+	expected := []int{1, 5}
+	xo.ChoosePosition("Jay", 1)
+
+	actualJay := xo.ChoosePosition("Aun", 5)
+
+	if fmt.Sprintf("%v", expected) != fmt.Sprintf("%v", actualJay) {
+		t.Errorf("Shoud Be %v but got %v", expected, actualJay)
+	}
+}
