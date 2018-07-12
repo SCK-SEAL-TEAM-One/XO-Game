5c8b5341b8a9e6f79068aab0399855d0e3f6f193
nat.la <nat.la@baac.or.th>
2018-07-12 14:45:32 +0700

    [Created:] สร้าง map ของ Cell สำหรับแปลเลขเป็น Row&Col ในฟงักืชั่นInputPlayerO

--- diagonal-win/src/xo/xo.go
+++ diagonal-win/src/xo/xo.go
@@ -10,7 +10,29 @@ func NewXOGame() XOGAME{
 	return XOGAME{MaxRound:9} 
 } 
 
-func InputPlayerO() bool{
+type Cell struct {
+	Row int
+	Col int
+}
+func (xo *XOGAME) InputPlayerO(number int) bool{
+	numberCell := map[int] Cell{
+		1: Cell{ Row: 0, Col: 0 },
+		2: Cell{ Row: 0, Col: 1 },
+		3: Cell{ Row: 0, Col: 2 },
+		4: Cell{ Row: 1, Col: 0 },
+		5: Cell{ Row: 1, Col: 1 },
+		6: Cell{ Row: 1, Col: 2 },
+		7: Cell{ Row: 2, Col: 0 },
+		8: Cell{ Row: 2, Col: 1 },
+		9: Cell{ Row: 2, Col: 2 },
+	} 
+	
+	row := numberCell[number].Row
+	col := numberCell[number].Col
+	if xo.Table[row][col] == "" {
+		xo.Table[row][col] = "O"
+		return true
+	}
 	return false
 
 }
--- diagonal-win/src/xo/xo_test.go
+++ diagonal-win/src/xo/xo_test.go
@@ -6,9 +6,11 @@ import (
 
 func Test_InputPlayerO_Input_5_Should_Be_True(t *testing.T){
 	input := 5
-	expected := true 
-	actual := InputPlayerO() 
+	expected := true
+	xo := NewXOGame() 
+	actual := xo.InputPlayerO(input) 
 	if expected != actual {
-		t.Errorf("Should be %v but got %v ",expected, actual )
+
+		t.Errorf("Should be %v but got %v %v",expected, actual, xo.Table)
 	}
 }
