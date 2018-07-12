d20bc06c4b3bd5ac817f60985f62c85a4e998fd8
smalldog124 <panumars124@gmail.com>
2018-07-12 16:30:04 +0700

    [Edited] เพิ่มฟังก์ชั่น main ในไฟล์ main.go เพื่อเรียก ฟังก์ชั่น PlayGame

--- vertical-win/src/main.go
+++ vertical-win/src/main.go
@@ -7,5 +7,42 @@ import (
 )
 
 func main()  {
-    
+    board  :=[]model.Cell{}
+    playerWinner := ""
+
+    for playerWinner == "" {
+        fmt.Println("Player 1 Enter Row: ")
+        var row int
+        fmt.Scanln(&row)
+        fmt.Println("Player 1 Enter Column: ")
+        var column int
+        fmt.Scanln(&column)
+
+        if vertical.DuplicateMark(row,column,board) == false{
+            playerSelectCell := vertical.Mark("Player1",row,column)
+            board = append(board,playerSelectCell)
+            playerWinner = vertical.GetWinner(board)
+            fmt.Println("win",playerWinner)
+            vertical.CreateBoard(board)
+        }
+
+        fmt.Println("Player 2 Enter Row: ")
+        var rowPlayer2 int
+        fmt.Scanln(&rowPlayer2)
+        fmt.Println("Player 2 Enter Column: ")
+        var columnPlayer2 int
+        fmt.Scanln(&columnPlayer2)
+
+        if vertical.DuplicateMark(rowPlayer2,columnPlayer2,board) == false{
+            playerSelectCell := vertical.Mark("Player2",rowPlayer2,columnPlayer2)
+            board = append(board,playerSelectCell)
+            playerWinner = vertical.GetWinner(board)
+            fmt.Println("win",playerWinner)
+            fmt.Println(board)  
+            vertical.CreateBoard(board)
+        }
+
+    }
+
+    fmt.Println(playerWinner)
 }
